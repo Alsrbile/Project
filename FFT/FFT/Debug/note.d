16b768351c28a3dda5ad845a297e3ee5c6c802b9
송민규 <70309556+Alsrbile@users.noreply.github.com>
2023-02-12 20:23:32 +0900

    Delete FFT directory

--- FFT/FFT/Debug/note.d
+++
@@ -1,4 +0,0 @@
-note.d note.o: .././note.c \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
